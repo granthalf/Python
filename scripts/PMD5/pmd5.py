5bd5385affe789415b2328d1c04092177b59701c
granthalf <cmonk_csec@proton.me>
2026-03-25 09:31:23 +0100

    Add files via upload

---
+++ scripts/PMD5/pmd5.py
@@ -0,0 +1,19 @@
+#Use this website in order to test the strength of your hash:
+#https://md5.gromweb.com/
+import sys
+import hashlib
+
+def compute_md5(text: str) -> str:
+    """Return the MD5 hash of the given text."""
+    md5 = hashlib.md5()
+    md5.update(text.encode("utf-8"))
+    return md5.hexdigest()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python md5_hash.py <text>")
+        sys.exit(1)
+
+    input_text = sys.argv[1]
+    result = compute_md5(input_text)
+    print(f"MD5: {result}")
